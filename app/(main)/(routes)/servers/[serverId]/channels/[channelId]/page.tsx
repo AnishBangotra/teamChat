54018ddaa30653d8a37e59e259d1c1df3aca8205
AnishBangotra <abhibangotra76@gmail.com>
2023-09-16 19:50:14 +0530

    message api and socketIO setup

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -32,11 +33,22 @@ const ChannelIdPage = async ({
     if (!channel || !member) return redirect("/");
     
     return(
-        <div className="bg-white dark:bg-[#313338]">
+        <div className="bg-white dark:bg-[#313338]
+        flex flex-col h-full">
             <ChatHeader 
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">Future messages</div>
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     )
